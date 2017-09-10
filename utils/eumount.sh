0c834e35ff4cb39118834a92ef0302137bece3ea
jorgenbele <jorgen.bele@gmail.com>
2017-09-10 17:31:43 +0200

    Changed notification to use notification.sh.

--- utils/eumount.sh
+++ utils/eumount.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-# Author: Jørgen Bele Reinfjell 
-# Date: xx.xx.2016 [dd.mm.yyyy]
-# File: eumount.sh
-# Description: 
-#   Unmounts the encrypted secret directory 
-#   $HOME/.$SECRET_DIR from $HOME/$SECRET_DIR.
-# Dependencies: ecryptfs
-# TODO: Make SECRET_DIR an cli-option.
-# See: emount.sh
-
 SECRET_DIR="sec"
 # insert passphrase into keyring
 #ecryptfs-insert-wrapped-passphrase-into-keyring
@@ -18,7 +8,10 @@ sec_mnt="$(cat "$HOME/.ecryptfs/$SECRET_DIR.conf" | awk '{print $2}')"
 
 # unmount secret directory
 printf "Unmounting '$SECRET_DIR' at '$sec_mnt' "
-umount.ecryptfs_private "$SECRET_DIR" && printf "[SUCCESS]\n" || printf "[FAILURE]\n"
+umount.ecryptfs_private "$SECRET_DIR" && printf "[SUCCESS]\n" || printf "[FAILURE]\n" && exit 0
 
 # remove mount dir
 rmdir "$sec_mnt"
+
+notification -d "emount"
+notification -t 10 -k "eumount" -a "ecryptfs: unmounting..." &
