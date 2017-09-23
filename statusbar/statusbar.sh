27fba1885082f9bc576b404ab84a0d29f0bd0f68
jorgenbele <jorgen.bele@gmail.com>
2017-09-24 00:16:13 +0200

    Fixed mistake setting lemonbar_args to LEMONBAR_CMD.

--- statusbar/statusbar.sh
+++ statusbar/statusbar.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Author: Jørgen Bele Reinfjell
+# Author: Jørgen Bele Reinfjell 
 # Date: xx.07.2017 [dd.mm.yyyy]
 # File: statusbar.sh
-# Description:
+# Description: 
 #   Starts and runs the statusline generator status
 #   and pipes it's output into 'lemonbar'.
 # Dependencies: status, lemonbar
@@ -14,13 +14,13 @@
 [ -z "$STATUS_PID_FILE"  ] && STATUS_PID_FILE="/tmp/status_pid"
 
 [ -n "$LEMONBAR_CMD"  ] && lemonbar_cmd="$LEMONBAR_CMD" || lemonbar_cmd="lemonbar"
-[ -n "$LEMONBAR_ARGS" ] && lemonbar_args="$LEMONBAR_CMD"
+[ -n "$LEMONBAR_ARGS" ] && lemonbar_args="$LEMONBAR_ARGS"
 
 # Fontsize
 if [ -z "$FONTSIZE" ]; then
     case $# in
-        0)
-            # Restore font size
+        0) 
+            # Restore font size 
             [ -f "$FONT_SIZE_FILE" ] && NEWFONTSIZE="$(cat "$FONT_SIZE_FILE")"
             if [ -n "$NEWFONTSIZE" ]; then
                 FONTSIZE="$NEWFONTSIZE"
@@ -41,7 +41,7 @@ echo "FONT_SIZE_FILE=$FONT_SIZE_FILE" 1>&2
 echo "FONTSIZE=$FONTSIZE" 1>&2
 echo "STATUSBAR_FONT=$STATUSBAR_FONT" 1>&2
 
-# Save font size
+# Save font size 
 echo "$FONTSIZE" > "$FONT_SIZE_FILE"
 
 # Create named pipe if not already existing.
@@ -70,10 +70,10 @@ renice 19 -p "$status_pid"
 # Write pid to file.
 echo "$status_pid" > "$STATUS_PID_FILE"
 
-# Start 'lemonbar' with a low priority.
+# Start 'lemonbar' with a low priority. 
 # - 'lemonbar' will be killed when status is killed.
 echo "Starting lemonbar..." 1>&2
 nice -19 $lemonbar_cmd $lemonbar_args -f "$STATUSBAR_FONT" < "$STATUS_FIFO_PATH"
 
 # Start new process.
-#nice status 2>/dev/null | lemonbar -f "$STATUSBAR_FONT"
+#nice status 2>/dev/null | lemonbar -f "$STATUSBAR_FONT" 
