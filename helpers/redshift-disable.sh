f950b1cfca52cb42cc75d40891dc7fbdf7a49d51
jorgenbele <jorgen.bele@gmail.com>
2017-11-12 22:39:37 +0100

    Changed notification title/name.

--- helpers/redshift-disable.sh
+++ helpers/redshift-disable.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 notification -d "redshift"
-notification -t 10 -a "redshift: shutting down..." &
+notification -t 10 -a "rs: shutting down..." &
 
 redshift -x # restore colors and brightness
 killall -e redshift # kill running instances (if any)
--- helpers/redshift-enable.sh
+++ helpers/redshift-enable.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-notification -k "redshift" -a "redshift"
+notification -k "redshift" -a "rs"
 killall -e redshift # kill running instances (if any)
 redshift &     # start new instance
 
